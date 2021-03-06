e4c9e386347d5d857d9a184e8088ec80af8f79a5
Agustin Borda <agustinborda1998@gmail.com>
2021-03-06 01:01:27 -0300

    included eosim library, added Pedido's header
    Signed-off-by: Agustin Borda <agustinborda1998@gmail.com>

--- Ascensor/headers/pedido.hpp
+++ Ascensor/headers/pedido.hpp
@@ -5,10 +5,21 @@
 #include <eosim/core/entity.hpp>
 #include <string>
 
+class Pedido {
+    private:
+        int piso;
+    public:
+        Pedido();
+        ~Pedido();
+        int getPiso();
+        void setPiso(int newPiso);
+};
+
+
 // identificador del evento ligado PedidoFeeder
 const std::string pedidoF = "PedidoFeeder";
 
-class PedidoFeeder: public eosim::core::Bevent {
+class PedidoFeeder: public eosim::core::BEvent {
 public:
 	// constructor
 	PedidoFeeder(eosim::core::Model& model);
@@ -17,5 +28,6 @@ public:
 	// rutina del evento fijo
 	void eventRoutine(eosim::core::Entity* who);
 };
+
 
 #endif
