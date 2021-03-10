2cb8c5353fd4b52b2d011dbe2cd01e8eac9c171a
NicoFraschetti <nicolasfraschetti@gmail.com>
2021-03-09 21:18:52 -0300

    requests feeder added

--- Ascensor/src/pedido.cpp
+++ Ascensor/src/pedido.cpp
@@ -1,5 +1,8 @@
 #include "../headers/pedido.hpp"
 #include <stdlib.h>
+
+// Definicion de la entidad pedido
+
 Pedido::Pedido() {
     piso = rand() % 10;
 }
@@ -12,4 +15,19 @@ int Pedido::getPiso() {
 
 void Pedido::setPiso(int newPiso) {
     piso = newPiso;
+}
+
+// Definicion del evento ligado PedidoFeeder
+
+PedidoFeeder::PedidoFeeder(Model& model): BEvent(pedidoF, model) {}
+
+PedidoFeeder::~PedidoFeeder() {}
+
+void PedidoFeeder::eventRoutine(Entity* who) {
+	// se anuncia la llegada del paciente
+	std::cout << "llego un pedido en " << who->getClock() << "\n";
+	// se castea owner a un HospitalSimple
+	Ascensor& ascensor = dynamic_cast<Ascensor&>(owner);
+	ascensor.q0.push(who);
+	ascensor.schedule(ascensor.arribos.sample(), new Pedido(), pedidoF);
 }
