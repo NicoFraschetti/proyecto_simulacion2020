f0abad2720bea72353e1e582b7aec67ad0e41b02
Agustin Borda <agustinborda1998@gmail.com>
2021-03-11 18:16:47 -0300

    added src code for model
    Signed-off-by: Agustin Borda <agustinborda1998@gmail.com>

--- Ascensor/src/ascensor.cpp
+++ Ascensor/src/ascensor.cpp
@@ -1,2 +1,31 @@
 #include "../headers/ascensor.hpp"
+#include <eosim/core/entity.hpp>
+#include <eosim/dist/numbergenerator.hpp>
+#include <iostream>
+using namespace eosim::core;
+using namespace eosim::dist;
 
+using namespace std;
+
+Ascensor::Ascensor(double tasaArribos):
+            tasaArribos(tasaArribos),
+            pF(*this),
+            dA(*this),
+            arribos(MT19937, tasaArribos),
+            libreAscensor1(1, 1),
+            libreAscensor2(1, 1) {}
+
+Ascensor::~Ascensor() {}
+
+void Ascensor::init() {
+    registerBEvent(&pF);
+
+    registerCEvent(&dA);
+
+    registerDist(&arribos);
+}
+
+
+void Ascensor::doInitialSchedules() {
+    schedule(0.0, new Pedido(), pedidoF);
+}
